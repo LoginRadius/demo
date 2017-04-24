36e0b828af146f1b14254151f75749df71dcfbd9
Karl-LoginRadius <feorn16@gmail.com>
2017-04-24 10:11:20 -0700

    V2 Basic Demo

--- option.js
+++ option.js
@@ -1,11 +1,9 @@
 
-var option = {};
-option.apikey = "<LoginRadius-API-Key>";
-option.appName = "<LoginRadius-Site-Name>";
-option.V2Recaptcha = true;
-option.inFormvalidationMessage = true;
-option.enableLoginOnEmailVerification = true;
-/** You can directly bind it with the correct url, the string operation is just to dynamically bind them**/
-var path = window.location.href;
-option.emailVerificationUrl = path.replace(path.substr(path.lastIndexOf('/')), "/email-verification.html");
-option.forgotPasswordUrl = path.replace(path.substr(path.lastIndexOf('/')), "/reset-password.html");
+var commonOptions = {};
+commonOptions.apiKey = "<LoginRadius API Key>";
+commonOptions.appName = "<LoginRadius Site Name>";
+commonOptions.hashTemplate= true;
+commonOptions.sott ="<Sott>";
+commonOptions.verificationUrl = window.location;//Change as per requirement
+
+var LRObject= new LoginRadiusV2(commonOptions);
