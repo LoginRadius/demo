42257a15033a1c2a9bc1d0709cbcd827f1ca0653
Karl-LoginRadius <feorn16@gmail.com>
2017-06-26 08:37:39 -0700

    Update redirect urls

--- option.js
+++ option.js
@@ -4,6 +4,6 @@ commonOptions.apiKey = "<LoginRadius API Key>";
 commonOptions.appName = "<LoginRadius Site Name>";
 commonOptions.hashTemplate= true;
 commonOptions.sott ="<Sott>";
-commonOptions.verificationUrl = window.location;//Change as per requirement
-
+commonOptions.verificationUrl = path.replace(path.substr(path.lastIndexOf('/')), "/email-verification.html");
+commonOptions.resetPasswordUrl = path.replace(path.substr(path.lastIndexOf('/')), "/reset-password.html");
 var LRObject= new LoginRadiusV2(commonOptions);
