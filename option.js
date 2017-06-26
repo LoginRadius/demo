157ed676e16167c5ce8aacf18708e44fa4f44d33
Karl-LoginRadius <feorn16@gmail.com>
2017-06-26 08:43:08 -0700

    Add Path

--- option.js
+++ option.js
@@ -4,6 +4,7 @@ commonOptions.apiKey = "<LoginRadius API Key>";
 commonOptions.appName = "<LoginRadius Site Name>";
 commonOptions.hashTemplate= true;
 commonOptions.sott ="<Sott>";
+var path = window.location.href;
 commonOptions.verificationUrl = path.replace(path.substr(path.lastIndexOf('/')), "/email-verification.html");
 commonOptions.resetPasswordUrl = path.replace(path.substr(path.lastIndexOf('/')), "/reset-password.html");
 var LRObject= new LoginRadiusV2(commonOptions);
