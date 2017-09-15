c01f01bfb1025008b1fc694efbd65ee0159da117
lr-pascal <pascal@loginradius.com>
2017-09-15 16:22:38 -0700

    add YOLO demo

--- option.js
+++ option.js
@@ -8,3 +8,9 @@ var path = window.location.href;
 commonOptions.verificationUrl = path.replace(path.substr(path.lastIndexOf('/')), "/email-verification.html");
 commonOptions.resetPasswordUrl = path.replace(path.substr(path.lastIndexOf('/')), "/reset-password.html");
 var LRObject= new LoginRadiusV2(commonOptions);
+var sdkoptions = {
+
+
+  "key": "<LoginRadius API Key>"
+
+}
