64ae8ace6f70c6dd8e6971a5939fbbc1b87d9ecc
Shumatsu Mokuyobi <shumatsum@protonmail.ch>
2020-10-01 08:39:29 +0200

    Minor fixes
    New line at the end of every file, removed extra empty lines at the beginning of files, fixed spacing in js file

--- option.js
+++ option.js
@@ -1,10 +1,9 @@
-
 var commonOptions = {};
 commonOptions.apiKey = "<LoginRadius API Key>";
 commonOptions.appName = "<LoginRadius Site Name>";
-commonOptions.hashTemplate= true;
-commonOptions.sott ="<Sott>";
+commonOptions.hashTemplate = true;
+commonOptions.sott = "<Sott>";
 var path = window.location.href;
 commonOptions.verificationUrl = path.replace(path.substr(path.lastIndexOf('/')), "/email-verification.html");
 commonOptions.resetPasswordUrl = path.replace(path.substr(path.lastIndexOf('/')), "/reset-password.html");
-var LRObject= new LoginRadiusV2(commonOptions);
+var LRObject = new LoginRadiusV2(commonOptions);
